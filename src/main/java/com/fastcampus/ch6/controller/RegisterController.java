910b5986685f8649ebaf64a70ccfbc58752a112f
Reprisal123123 <kezy@daum.net>
2022-05-19 23:31:43 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/ch6/controller/RegisterController.java
@@ -0,0 +1,24 @@
+package com.fastcampus.ch6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+
+    @GetMapping("/add")
+    public String register() {
+        return "registerForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+
+        return "registerInfo";
+    }
+
+}
