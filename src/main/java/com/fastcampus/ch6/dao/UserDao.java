7a45780ed72e0ab6b970c2169e79c75dd13696fa
Reprisal123123 <kezy@daum.net>
2022-05-20 01:00:25 +0900

    create UserDao.java, userMapper.xml

--- src/main/java/com/fastcampus/ch6/dao/UserDao.java
+++ src/main/java/com/fastcampus/ch6/dao/UserDao.java
@@ -5,6 +5,11 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class UserDao {
 
@@ -13,14 +18,35 @@ public class UserDao {
 
     private String namespace = "com.fastcampus.ch6.dao.UserMapper.";
 
-    public int insert(UserDto userDto) {
+    public int insert(UserDto userDto) throws Exception {
         return session.insert(namespace+"insert", userDto);
     }
 
-    public UserDto select(String id) {
+    public UserDto select(String id) throws Exception {
         return session.selectOne(namespace+"select", id);
     }
 
+    public List<UserDto> selectAll() throws Exception {
+        return session.selectList(namespace+"selectAll");
+    }
 
+    public int count() throws Exception {
+        return session.selectOne(namespace+"count");
+    }
+
+    public int update(UserDto userDto) throws Exception {
+        return session.update(namespace+"update", userDto);
+    }
+
+    public int delete(String id, String pwd) throws Exception {
+        Map map = new HashMap();
+        map.put("id", id);
+        map.put("pwd", pwd);
+        return session.delete(namespace+"delete", map);
+    }
+
+    public int deleteAll() throws Exception {
+        return session.delete(namespace+"deleteAll");
+    }
 
 }
